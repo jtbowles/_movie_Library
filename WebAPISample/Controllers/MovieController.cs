92709fa4b77c7df62ef5fa2d9b84bad3c60f5a41
Jason Bowles <jbowles96@gmail.com>
2019-10-08 15:17:45 -0500

    Logic for REST API GET PUT POST | Todo: Test REST API through postman

--- WebAPISample/Controllers/MovieController.cs
+++ WebAPISample/Controllers/MovieController.cs
@@ -17,36 +17,77 @@ namespace WebAPISample.Controllers
             context = new ApplicationDbContext();
         }
 
-        // GET api/values
-        public IEnumerable<string> Get()
+        // GET api/movie
+        public IEnumerable<Movie> GetMovies()
         {
-            // Retrieve all movies from db logic
-            return new string[] { "movie1 string", "movie2 string" };
+            return context.Movies.ToList();
         }
 
-        // GET api/values/5
-        public string Get(int id)
+        // GET api/movies/1
+        public Movie GetMovie(int id)
         {
             // Retrieve movie by id from db logic
-            return "value";
+            var movie = context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return movie;
         }
 
-        // POST api/values
-        public void Post([FromBody]Movie value)
+        // POST api/movies
+        [HttpPost]
+        public Movie CreateMovie(/*[FromBody]*/Movie movie)
         {
             // Create movie in db logic
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            context.Movies.Add(movie);
+            context.SaveChanges();
+
+            return movie;
         }
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        // PUT api/movies/1
+        [HttpPut]
+        public void UpdateMovie(int id, /*[FromBody]string value*/ Movie movie)
         {
             // Update movie in db logic
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var movieInDb = context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if(movieInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            movieInDb.Title = movie.Title;
+            movieInDb.Genre = movie.Genre;
+            movieInDb.DirectorName = movie.DirectorName;
+
+            context.SaveChanges();
         }
 
-        // DELETE api/values/5
-        public void Delete(int id)
+        // DELETE api/movies/1
+        public void DeleteMovie(int id)
         {
             // Delete movie from db logic
+            var movieInDb = context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movieInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            context.Movies.Remove(movieInDb);
+            context.SaveChanges();
         }
     }
 
