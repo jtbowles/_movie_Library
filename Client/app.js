4253196ae19d3bcd65ef7a02a7db2be70bed0cfd
Jason Bowles <jbowles96@gmail.com>
2019-10-11 09:37:20 -0500

    final bootstrap touchups before submission | Todo: complete functionality for update button and PUT method

--- Client/app.js
+++ Client/app.js
@@ -14,15 +14,7 @@
             contentType: 'application/json',
             data: JSON.stringify(dict),
             success: function( data, textStatus, jQxhr ){
-                //MovieStuff = data;
-                //$(".movieData").empty();
-                //console.log(data);
                 populateMovieLibrary();
-                //.then()
-
-                // remove the existing table to avoid duplicate tables
-                //populateTable();
-                //$('#response pre').html( data );
             },
             error: function( jqXhr, textStatus, errorThrown ){
                 console.log( errorThrown );
@@ -43,15 +35,11 @@
                 $(".movieData").empty();
                 $.each(data , function(index, value){
                     $('.movieData').append(
-                        "<tr>" +
+                        "<tr class='info'>" +
                         "<td>" + value.Title + "</td>" + 
                         "<td>" + value.DirectorName + "</td>" +
                         "<td>" + value.Genre + "</td>" +
-                        // "<td><button id='updatemovie' class='btn-small' type='button' >Edit</button></td>" +
                         "</tr>");
-
-
-                    console.log(value);
                 })
             },
             error: function( jqXhr, textStatus, errorThrown ){
@@ -71,11 +59,11 @@
                 $(".movieData").empty();
                 $.each(data , function(index, value){
                     $('.movieData').append(
-                        "<tr>" +
+                        "<tr class='warning'>" +
                         "<td class='movie-title' contenteditable='true'>" + value.Title + "</td>" + 
                         "<td class='movie-director' contenteditable='true'>" + value.DirectorName + "</td>" +
                         "<td class='movie-genre' contenteditable='true'>" + value.Genre + "</td>" +
-                        "<td><button class='btn-small' type='submit' onclick='getMovie(" + value.Id + ")' >Update</button></td>" +
+                        "<td><button class='btn btn-warning' type='button' onclick='getMovie(" + value.Id + ")' >Update</button></td>" +
                         "</tr>");
 
 
@@ -89,27 +77,9 @@
     }
 
 
-    function updateMovieLibrary(){
-        console.log('success');
-        // $.ajax({
-        //     // url: 'https://localhost:44352/api/movie/' + id,
-        //     dataType: 'json',
-        //     type: 'get',    
-        //     contentType: 'application/json',
-        //     success: function( data, textStatus, jQxhr ){
-        //         console.log("success");
-        //     },
-        //     error: function( jqXhr, textStatus, errorThrown ){
-        //         console.log( errorThrown );
-        //     }
-        // });
-    }
-
-
     $('#my-form').submit( processForm );
     $('#getmovies').on('click', populateMovieLibrary);
     $('#editmovies').on('click', editMovieLibrary);
-    $('#updatemovies').on('click', updateMovieLibrary);
 })(jQuery);
 
 
